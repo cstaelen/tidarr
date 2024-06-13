5553a0ace5e3a0a8ec797cb7b7ff70f9df08d9b8
cl.st <clement@cl-st.dev>
2024-06-13 23:42:27 +0200

    renovate: migrate to eslint 9

---
+++ e2e/eslint.config.mjs
@@ -0,0 +1,63 @@
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import typescriptEslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...typescriptEslint.configs.recommended,
+  {
+    ignores: [
+      "**/node_modules",
+      "**/playwright-report",
+    ],
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        tsconfigRootDir: "src/",
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "prettier/prettier": "error",
+
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // Packages `react` related packages come first.
+            ["^react", "^@?\\w"],
+            // Internal packages.
+            ["^(@|components)(/.*|$)"],
+            // Internal packages.
+            ["^(assets/js)(/.*|$)"],
+            // Side effect imports.
+            ["^\\u0000"],
+            // Parent imports. Put `..` last.
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            // Other relative imports. Put same-folder imports and `.` last.
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            // Style imports.
+            ["^.+\\.?(css)$"],
+          ],
+        },
+      ],
+
+      "simple-import-sort/exports": "error",
+    },
+  },
+];
