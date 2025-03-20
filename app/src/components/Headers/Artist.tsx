2f0604e536f73729a76ab1c0051307dc73ccde30
cl.st <clement@cl-st.dev>
2025-03-20 18:50:15 +0100

    feat: add get all message

--- app/src/components/Headers/Artist.tsx
+++ app/src/components/Headers/Artist.tsx
@@ -1,3 +1,4 @@
+import { Alert, AlertTitle } from "@mui/material";
 import { ArtistType } from "src/types";
 
 import { DownloadButton } from "../Buttons/DownloadButton";
@@ -6,22 +7,40 @@ import PageHeader from "./Header";
 
 export default function ArtistHeader({ artist }: { artist: ArtistType }) {
   return (
-    <PageHeader
-      title={artist.name}
-      url={artist.url}
-      image={`https://resources.tidal.com/images/${artist.picture?.replace(
-        /-/g,
-        "/",
-      )}/750x750.jpg`}
-      afterTitle={
-        <DownloadButton
-          item={artist}
-          id={artist.id}
-          type="artist"
-          label="Get all releases"
-        />
-      }
-      subtitle="Artist(s)"
-    />
+    <>
+      <PageHeader
+        title={artist.name}
+        url={artist.url}
+        image={`https://resources.tidal.com/images/${artist.picture?.replace(
+          /-/g,
+          "/",
+        )}/750x750.jpg`}
+        afterTitle={
+          <DownloadButton
+            item={artist}
+            id={artist.id}
+            type="artist"
+            label="Get all releases"
+          />
+        }
+        subtitle="Artist(s)"
+      />
+      <Alert color="info" sx={{ my: 2 }}>
+        <AlertTitle>"Get all releases" button.</AlertTitle>
+        By default, Singles and EP's are not concerned by the "Get all releases"
+        button.
+        <br />
+        To include them, please update "singles_filter" parameter in your
+        "tiddl.json" file.
+        <br />
+        <a
+          href="https://github.com/cstaelen/tidarr?tab=readme-ov-file#download-settings-optional"
+          target="_blank"
+        >
+          More details
+        </a>
+        .
+      </Alert>
+    </>
   );
 }
