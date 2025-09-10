803544dcc4068926ec24f1295420b3d870369327
cl.st <clement@cl-st.dev>
2025-09-10 11:52:45 +0200

    feat: add cache to proxy

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+import apicache from "apicache";
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
@@ -17,11 +18,13 @@ const port = 8484;
 const hostname = "0.0.0.0";
 
 const app: Express = express();
+const cache = apicache.middleware;
 
 app.use(express.json());
 app.use(cors());
 app.use(
   "/proxy",
+  cache("1 minute"),
   proxy(TIDAL_API_URL, {
     proxyReqOptDecorator: function (proxyReqOpts) {
       delete proxyReqOpts.headers["referer"];
