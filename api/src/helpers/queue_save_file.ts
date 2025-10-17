025a7356a44a141a4de81b057da56840262e35fe
cl.st <clement@cl-st.dev>
2025-10-17 17:13:02 +0200

    Feat/save processing list (#388)
    * feat: save queue to file
    
    * feat: update tiddl to 2.6.4

---
+++ api/src/helpers/queue_save_file.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+
+import { ROOT_PATH } from "../../constants";
+import { ProcessingItemType } from "../types";
+
+const filePath = path.join(`${ROOT_PATH}/shared`, "queue.json");
+
+export function loadQueueFromFile(): ProcessingItemType[] {
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, JSON.stringify([], null, 2));
+    return [];
+  } else {
+    return JSON.parse(fs.readFileSync(filePath, "utf8"));
+  }
+}
+
+export const addItemToFile = (item: ProcessingItemType) => {
+  const saveList: ProcessingItemType[] = JSON.parse(
+    fs.readFileSync(filePath, "utf8"),
+  );
+
+  saveList.push(item);
+  fs.writeFileSync(filePath, JSON.stringify(saveList, null, 2));
+};
+
+export const removeItemFromFile = (id: string) => {
+  let saveList: { id: string; url: string; contentType: string }[] = JSON.parse(
+    fs.readFileSync(filePath, "utf8"),
+  );
+
+  saveList = saveList.filter((item) => item.id !== id);
+  fs.writeFileSync(filePath, JSON.stringify(saveList, null, 2));
+};
